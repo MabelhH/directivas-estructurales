93f090a0ac94d89b1738768080de510e00df1838
clara23 <clarahuanaco20@gmail.com>
2023-01-03 14:04:27 -0500

    actualisacion 02

---
+++ src/app/angular/angular.module.ts
@@ -0,0 +1,23 @@
+import { Host, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+
+
+
+
+
+@NgModule({
+  declarations: [
+   
+  ],
+  imports: [
+    CommonModule,
+    
+  ],
+  exports:[
+   
+  ],
+  
+
+})
+export class AngularModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { formatCurrency, formatDate } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularModule } from './angular/angular.module';
+import { HomeComponent } from './angular/home/home.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
